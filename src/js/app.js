4104359d038f773b6e894011443ea28e92f8db0a
horse <littlehorse_boy@gmail.com>
2018-11-11 01:57:57 +0800

    feat: 卡片 3D 特效
    todo: 動畫 transition
    
    issue 3D

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,20 @@
 import '../source/style.scss';
+
+/* global Vue */
+new Vue({
+  el: '#app',
+  data: {
+    chapter: 'one',
+  },
+  methods: {
+    clickCard: function () {
+      if (this.chapter === 'one') {
+        this.chapter = 'two';
+      } else if (this.chapter === 'two') {
+        this.chapter = 'three';
+      } else if (this.chapter === 'three') {
+        this.chapter = 'one';
+      }
+    },
+  },
+});

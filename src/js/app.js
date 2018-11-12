a8bae13f546b27d5438cd8af59c239564fbef891
littlehorseboy <littlehorseboy@gmail.com>
2018-11-12 23:39:05 +0800

    feat: 卡片 3D 動畫特效
    文字區
    
    todo: 裝飾場景
    
    issue 3D

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,11 @@
 import '../source/style.scss';
+import anime from 'animejs';
 
 /* global Vue */
 new Vue({
   el: '#app',
   data: {
     chapter: 'one',
-    chapterThreeOpen: true,
   },
   methods: {
     clickCard: function () {
@@ -13,8 +13,51 @@ new Vue({
         this.chapter = 'two';
       } else if (this.chapter === 'two') {
         this.chapter = 'three';
-      } else if (this.chapter === 'three') {
-        this.chapterThreeOpen = !this.chapterThreeOpen;
+
+        this.$nextTick(function () {
+          anime({
+            targets: this.$refs.gift,
+            scale: 1.2,
+            rotate: {
+              value: 360,
+              duration: 1800,
+              easing: 'easeInOutSine',
+            },
+            delay: 250,
+          });
+
+          const animateButton = (scale, duration, elasticity) => {
+            anime.remove(this.$refs.gift);
+            anime({
+              targets: this.$refs.gift,
+              scale: scale,
+              duration: duration,
+              elasticity: elasticity,
+            });
+          };
+
+          const enterButton = () => {
+            animateButton(1.4, 800, 400);
+          };
+
+          const leaveButton = () => {
+            animateButton(1.2, 600, 300);
+          };
+
+          setTimeout(() => {
+            this.$refs.gift.addEventListener('mouseenter', enterButton, false);
+            this.$refs.gift.addEventListener('mouseleave', leaveButton, false);
+
+            this.$refs.gift.addEventListener('click', () => {
+              anime({
+                targets: this.$refs.paper,
+                opacity: 0.9,
+                scale: 0.8,
+                delay: 50,
+              });
+            });
+          }, 1800);
+        });
       }
     },
   },
